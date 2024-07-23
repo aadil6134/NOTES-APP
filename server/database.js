bf43d4741220e3670a50762e735f94f6fa9489c8
aadil <aadil6134@gmail.com>
2024-07-23 20:21:41 +0530

    initial commit

--- server/database.js
+++ server/database.js
@@ -1,19 +1,22 @@
-const sqlite3 = require('sqlite3').verbose()
-const dbName = 'myDatabase.db'
+const sqlite3 = require("sqlite3").verbose();
+const dbName = "myDatabase.db";
 
 const db = new sqlite3.Database(dbName, (err) => {
-    if(err){
-        console.error(err.message);
-    }else{
-        console.log("Connected to the Database");
-        db.run('CREATE TABLE IF NOT EXISTS notes(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT, note TEXT)', (err) => {
-            if(err){
-                console.error(err.message);
-            }else{
-                console.log("Table created successfully!!!");
-            }
-        })
-    }
-})
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log("Connected to the Database");
+    db.run(
+      "CREATE TABLE IF NOT EXISTS notes(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT, note TEXT)",
+      (err) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          console.log("Table created successfully!!!");
+        }
+      }
+    );
+  }
+});
 
 module.exports = db;
